88e3269bf389fad204fb5468c41a9f6a3699d386
marko marongiu <coentro07@gmail.com>
2024-04-19 09:25:10 +0200

    Aggiungere i file di progetto.

---
+++ AS2324.3G.MarongiuMarco.battagliaNavale/Form1.cs
@@ -0,0 +1,10 @@
+namespace AS2324._3G.MarongiuMarco.battagliaNavale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
